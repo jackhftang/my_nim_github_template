8ac9bfd1e2d47db29eac5bbe12f7c0ad568ecdf0
Jack Tang <1190545+jackhftang@users.noreply.github.com>
2023-03-15 08:51:12 +0800

    support multiple srcPath in  test

--- tests/test_src.nim
+++ tests/test_src.nim
@@ -4,13 +4,16 @@ macro runAllTests(): untyped =
   ## include all file under src with suffix _test.nim
   let testsPath = currentSourcePath()
   let rootPath = testsPath.parentDir.parentDir
-  let srcPath = joinPath(rootPath, "src")
-
+  let srcPaths = [
+    joinPath(rootPath, "src"),
+    joinPath(rootPath, "tests")
+  ]
   result = newStmtList()
-  for path in walkDirRec(srcPath, relative = true):
-    if path.endsWith "_test.nim":
-      let p = "../src/" & path
-      echo "include " & p
-      result.add nnkIncludeStmt.newTree(ident(p))
+  for srcPath in srcPaths:
+    for path in walkDirRec(srcPath, relative = true):
+      if path.endsWith "_test.nim":
+        let p = srcPath / path
+        echo "include " & p
+        result.add nnkIncludeStmt.newTree(ident(p))
 
 runAllTests()
